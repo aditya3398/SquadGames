fe7720483d0016f150c2de1d7c8a843bb794ff0a
Aditya <aditya.nadkarni12@gmail.com>
2017-08-23 20:41:07 -0400

    changes

--- app/src/main/java/com/adhass/creators/squadgames/controllers/SignInActivity.java
+++ app/src/main/java/com/adhass/creators/squadgames/controllers/SignInActivity.java
@@ -4,8 +4,6 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import com.adhass.creators.squadgames.R;
 
